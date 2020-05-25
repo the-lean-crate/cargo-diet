9ca19fd6b7153a1fbfec7817fc7a2d03aa907565
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-05-25 17:01:48 +0800

    more tests

--- tests/stateless-journey.sh
+++ tests/stateless-journey.sh
@@ -31,29 +31,49 @@ function remove_paths() {
   )
 )
 
-(sandbox
+(with "a cargo user and email"
   export CARGO_NAME=author
   export CARGO_EMAIL=author@example.com
 
-  (with "a newly initialized cargo project"
-    step "init cargo project" &&
-      expect_run ${SUCCESSFULLY} cargo init --name library --bin
+  (sandbox
+    (with "a newly initialized cargo project"
+      step "init cargo project" &&
+        expect_run ${SUCCESSFULLY} cargo init --name library --bin
 
-    it "runs successfully" && {
-      WITH_SNAPSHOT="$snapshot/success-include-directive-in-new-project" \
-      expect_run ${SUCCESSFULLY} "$exe" diet
-    }
+      it "runs successfully" && {
+        WITH_SNAPSHOT="$snapshot/success-include-directive-in-new-project" \
+        expect_run ${SUCCESSFULLY} "$exe" diet
+      }
 
-    it "modifies Cargo.toml to contain an include directive" && {
-      expect_snapshot "$snapshot/success-include-directive-in-new-project-cargo-toml" "Cargo.toml"
-    }
-
-    (when "running it again" &&
-      step "(run it one more time)" && expect_run ${SUCCESSFULLY} "$exe" diet
-
-      it "produces exactly the same output as before" && {
+      it "modifies Cargo.toml to contain an include directive" && {
         expect_snapshot "$snapshot/success-include-directive-in-new-project-cargo-toml" "Cargo.toml"
       }
+
+      (when "running it again" &&
+        it "runs successfully" && {
+          WITH_SNAPSHOT="$snapshot/success-include-directive-in-new-project" \
+          expect_run ${SUCCESSFULLY} "$exe" diet
+        }
+
+        it "produces exactly the same output as before" && {
+          expect_snapshot "$snapshot/success-include-directive-in-new-project-cargo-toml" "Cargo.toml"
+        }
+      )
+
+      (with "a new test file which is part of the src/ directory"
+        touch src/lib_test.rs
+
+        (when "running it again" &&
+          it "runs successfully" && {
+            WITH_SNAPSHOT="$snapshot/success-include-directive-in-new-project-test-added" \
+            expect_run ${SUCCESSFULLY} "$exe" diet
+          }
+
+          it "produces a new include directive which explicilty excludes the new file type" && {
+            expect_snapshot "$snapshot/success-include-directive-in-new-project-cargo-toml-with-tests-excluded" "Cargo.toml"
+          }
+        )
+      )
     )
   )
 )
