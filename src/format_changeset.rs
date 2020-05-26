8c8fb13ddea6d9bd3359d711a3c0931edc3df4ff
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-05-26 13:57:39 +0800

    working context printing

--- src/format_changeset.rs
+++ src/format_changeset.rs
@@ -19,8 +19,30 @@ macro_rules! paint {
 const SIGN_RIGHT: char = '+'; // + > →
 const SIGN_LEFT: char = '-'; // - < ←
 
+fn lines_of(d: &Difference) -> impl Iterator<Item = &str> {
+    let buf = match d {
+        Difference::Same(b) | Difference::Rem(b) | Difference::Add(b) => b,
+    };
+    buf.split('\n')
+}
+
+fn print_context<'a>(
+    lines: Option<impl Iterator<Item = &'a str>>,
+    f: &mut impl std::io::Write,
+) -> std::io::Result<()> {
+    match lines {
+        None => {}
+        Some(lines) => {
+            for line in lines {
+                writeln!(f, "{}", line)?;
+            }
+        }
+    };
+    Ok(())
+}
+
 pub fn format_changeset(
-    mut f: impl std::io::Write,
+    mut t: impl std::io::Write,
     use_color: bool,
     changeset: &Changeset,
 ) -> std::io::Result<()> {
@@ -28,7 +50,7 @@ pub fn format_changeset(
 
     if use_color {
         writeln!(
-            f,
+            t,
             "{} {} / {} :",
             Style::new().bold().paint("Diff"),
             Red.paint(format!("{} left", SIGN_LEFT)),
@@ -36,7 +58,7 @@ pub fn format_changeset(
         )?;
     } else {
         writeln!(
-            f,
+            t,
             "{} {} / {} :",
             "Diff",
             format!("{} left", SIGN_LEFT),
@@ -44,20 +66,49 @@ pub fn format_changeset(
         )?;
     }
 
-    let mut t = f;
+    let is_different = |d: &_| {
+        if let Difference::Same(_) = d {
+            false
+        } else {
+            true
+        }
+    };
 
-    for i in 0..diffs.len() {
-        match diffs[i] {
-            Difference::Same(ref x) => {
-                writeln!(t, " {}", x)?;
-            }
-            Difference::Add(ref x) => {
-                paint!(t, use_color, Green, "{}{}\n", SIGN_RIGHT, x);
-            }
-            Difference::Rem(ref x) => {
-                paint!(t, use_color, Red, "{}{}\n", SIGN_LEFT, x);
+    let first_change = diffs.iter().position(is_different);
+    let last_change = diffs.iter().rposition(is_different);
+    let context = 2;
+
+    {
+        let one_before_change = first_change.map(|l| l.saturating_sub(1));
+        print_context(
+            one_before_change
+                .map(|l| lines_of(&diffs[l]).skip(diffs.len().saturating_sub(context))),
+            &mut t,
+        )?;
+    }
+
+    if let (Some(first_change), Some(last_change)) = (first_change, last_change) {
+        for i in first_change..=last_change {
+            match &diffs[i] {
+                Difference::Same(x) => {
+                    writeln!(t, " {}", x)?;
+                }
+                Difference::Add(x) => {
+                    paint!(t, use_color, Green, "{}{}\n", SIGN_RIGHT, x);
+                }
+                Difference::Rem(x) => {
+                    paint!(t, use_color, Red, "{}{}\n", SIGN_LEFT, x);
+                }
             }
         }
+    }
+
+    {
+        let one_after_last_change = last_change.map(|l| (l + 1).min(diffs.len()));
+        print_context(
+            one_after_last_change.map(|l| lines_of(&diffs[l]).take(context)),
+            &mut t,
+        )?;
     }
     Ok(())
 }
